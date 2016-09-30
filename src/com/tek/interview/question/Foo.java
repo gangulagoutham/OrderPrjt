735f777a000993d67794b1856824f6ab54ee68a0
Goutham Gangula <Goutham Gangula>
2016-09-30 00:56:37 -0500

    Bug6:used treemap for sorting orders

--- src/com/tek/interview/question/Foo.java
+++ src/com/tek/interview/question/Foo.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /* ****************************************************************************************
  
@@ -190,7 +191,8 @@ public class Foo {
 
 	public static void main(String[] args) throws Exception {
 
-		Map<String, Order> o = new HashMap<String, Order>();
+		//bug6 used TreeMap for sorting orders
+		Map<String, Order> o = new TreeMap<String, Order>();
 
 		Order c1 = new Order();
 
